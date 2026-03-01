ee90eb1da7a1e03814ae1b53996c1ad70efb1dbb
asmaAouadi <133607311+asmaAouadi@users.noreply.github.com>
2026-03-01 01:52:25 +0100

    correcting the sign up

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -7,8 +7,13 @@ import { useRouter } from 'next/navigation';
 export default function LoginPage() {
   const router = useRouter();
   const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [password1, setPassword1] = useState('');
   const [isLoading, setIsLoading] = useState(false);
+  const [name , setName]= useState('');
+  const [lastName, setLastName]= useState('');
+  const [phoneNumber , setPhoneNumber] = useState('');
+  const [password2, setPassword2] = useState('');
+
 
   const handleSignIn = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -37,12 +42,31 @@ export default function LoginPage() {
         <div className="w-full p-4 rounded-3xl bg-gradient-to-b from-[rgba(24,24,27,0.95)] to-[rgba(24,24,27,0.88)] backdrop-blur-xl border border-white/5 shadow-2xl">
           {/* Title */}
           <div className="text-center mb-6">
-            <h1 className="text-lg font-semibold text-white tracking-wide">Welcome back</h1>
+            <h1 className="text-lg font-semibold text-white tracking-wide">Hi there !</h1>
           </div>
 
           {/* Form */}
           <form onSubmit={handleSignIn} className="space-y-3">
-            {/* Email */}
+            <div className="space-y-1.5">
+              <label className="text-xs text-gray-400">Name</label>
+              <input
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                placeholder=""
+                className="w-full px-3 py-2 rounded-2xl bg-[rgba(39,39,42,0.9)] text-white placeholder-gray-500 text-xs border border-white/5 shadow-inner focus:outline-none focus:ring-1 focus:ring-blue-500/50 transition-all"
+              />
+            </div>
+            <div className="space-y-1.5">
+              <label className="text-xs text-gray-400">LastName</label>
+              <input
+                type="text"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+                placeholder=""
+                className="w-full px-3 py-2 rounded-2xl bg-[rgba(39,39,42,0.9)] text-white placeholder-gray-500 text-xs border border-white/5 shadow-inner focus:outline-none focus:ring-1 focus:ring-blue-500/50 transition-all"
+              />
+            </div>
             <div className="space-y-1.5">
               <label className="text-xs text-gray-400">Email</label>
               <input
@@ -53,18 +77,41 @@ export default function LoginPage() {
                 className="w-full px-3 py-2 rounded-2xl bg-[rgba(39,39,42,0.9)] text-white placeholder-gray-500 text-xs border border-white/5 shadow-inner focus:outline-none focus:ring-1 focus:ring-blue-500/50 transition-all"
               />
             </div>
+            {/* Email */}
+            <div className="space-y-1.5">
+              <label className="text-xs text-gray-400">Phone number</label>
+              <input
+                type="number"
+                value={phoneNumber}
+                onChange={(e) => setPhoneNumber(e.target.value)}
+                placeholder="+213"
+                className="w-full px-3 py-2 rounded-2xl bg-[rgba(39,39,42,0.9)] text-white placeholder-gray-500 text-xs border border-white/5 shadow-inner focus:outline-none focus:ring-1 focus:ring-blue-500/50 transition-all"
+              />
+            </div>
 
             {/* Password */}
             <div className="space-y-1.5">
               <label className="text-xs text-gray-400">Password</label>
               <input
                 type="password"
-                value={password}
+                value={password1}
+                onChange={(e) => getPassword(e.target.value)} // a function to confirm the password with the next one
+                placeholder="••••••••"
+                className="w-full px-3 py-2 rounded-2xl bg-[rgba(39,39,42,0.9)] text-white placeholder-gray-500 text-xs border border-white/5 shadow-inner focus:outline-none focus:ring-1 focus:ring-blue-500/50 transition-all"
+              />
+            </div>
+            <div className="space-y-1.5">
+              <label className="text-xs text-gray-400">Password</label>
+              <input
+                type="password"
+                value={password2}
                 onChange={(e) => setPassword(e.target.value)}
                 placeholder="••••••••"
                 className="w-full px-3 py-2 rounded-2xl bg-[rgba(39,39,42,0.9)] text-white placeholder-gray-500 text-xs border border-white/5 shadow-inner focus:outline-none focus:ring-1 focus:ring-blue-500/50 transition-all"
               />
             </div>
+
+
 
             {/* Sign In Button */}
             <button
