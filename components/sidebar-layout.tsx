2cf9bb17e922d65efb132f27538e4604c452d785
MorenaDev <133607311+asmaAouadi@users.noreply.github.com>
2026-03-07 13:40:55 +0100

    fixing missing side bar nev

--- components/sidebar-layout.tsx
+++ components/sidebar-layout.tsx
@@ -5,6 +5,7 @@ import { SidebarMenu } from "./sidebar";
 import { MdOutlinePostAdd } from "react-icons/md";
 import { IoIosLogOut } from "react-icons/io";
 import { FaHome, FaUser, FaCog } from "react-icons/fa";
+import { AiFillMessage } from 'react-icons/ai';
 
 interface SidebarLayoutProps {
   children: React.ReactNode;
@@ -12,11 +13,12 @@ interface SidebarLayoutProps {
 
 export const SidebarLayout: React.FC<SidebarLayoutProps> = ({ children }) => {
   const menuItems = [
-    { label: "Home", path: "/", icon: <FaHome /> },
-    { label: "Add Post", path: "/adding-post", icon: <MdOutlinePostAdd /> },
-    { label: "Profile", path: "/profile", icon: <FaUser /> },
-    { label: "Settings", path: "/settings", icon: <FaCog /> },
-    { label: "Log Out", path: "/logout", icon: <IoIosLogOut /> },
+    { label: 'Home', path: '/', icon: <FaHome /> },
+    { label: 'Add Post', path: '/adding-post', icon: <MdOutlinePostAdd /> },
+    { label: 'Profile', path: '/profile', icon: <FaUser /> },
+    { label: 'Messages', path: '/messages', icon: <AiFillMessage /> },
+    { label: 'Settings', path: '/settings', icon: <FaCog /> },
+    { label: 'Log Out', path: '#', icon: <IoIosLogOut />, onClick: handleLogoutClick },
   ];
 
   return (
