dc7ac5a62d7d420794dbe3bd9bfdf6d60a6715c8
MorenaDev <133607311+asmaAouadi@users.noreply.github.com>
2026-07-21 22:59:44 +0100

    fix: server.js delegates to standalone/server.js

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
-const { createServer } = require('http');
-const next = require('next');
+const fs = require('fs');
+const path = require('path');
 
-const app = next({ dev: false });
-const handle = app.getRequestHandler();
+const standaloneServer = path.join(__dirname, 'standalone', 'server.js');
 
-app.prepare().then(() => {
-  createServer((req, res) => handle(req, res)).listen(process.env.PORT || 3000);
-});
+if (fs.existsSync(standaloneServer)) {
+  require(standaloneServer);
+} else {
+  const { createServer } = require('http');
+  const next = require('next');
+
+  const app = next({ dev: false });
+  const handle = app.getRequestHandler();
+
+  app.prepare().then(() => {
+    createServer((req, res) => handle(req, res)).listen(process.env.PORT || 3000);
+  });
+}
+
