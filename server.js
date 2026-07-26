9270ddf462de718534132775b70f78c0a7adbb2c
MorenaDev <133607311+asmaAouadi@users.noreply.github.com>
2026-07-26 21:59:18 +0100

    fix: update root server.js and deploy.php wrapper to use nativeRequire

--- server.js
+++ server.js
@@ -5,17 +5,16 @@ const Module = require('module');
 const standaloneDir = path.join(__dirname, 'standalone');
 const standaloneNodeModules = path.join(standaloneDir, 'node_modules');
 
-// 1. Force standalone/node_modules to the top of module resolution
 process.env.NODE_PATH = standaloneNodeModules + path.delimiter + (process.env.NODE_PATH || '');
-Module._initPaths();
 
-// 2. Intercept require calls to force loading from standalone/node_modules
 const originalRequire = Module.prototype.require;
+const nativeRequire = Module.createRequire ? Module.createRequire(__filename) : originalRequire;
+
 Module.prototype.require = function(request) {
   if (request === 'next' || request.startsWith('next/')) {
     const nextStandalonePath = path.join(standaloneNodeModules, request);
     try {
-      return originalRequire.call(this, nextStandalonePath);
+      return nativeRequire.call(this, nextStandalonePath);
     } catch (e) {}
   }
   if (typeof request === 'string' && (request.startsWith('./') || request.startsWith('../'))) {
@@ -24,35 +23,23 @@ Module.prototype.require = function(request) {
       const absPath = path.resolve(parentDir, request);
       if (!fs.existsSync(absPath)) {
         if (fs.existsSync(absPath + '.js')) {
-          return originalRequire.call(this, absPath + '.js');
+          return nativeRequire.call(this, absPath + '.js');
         } else if (fs.existsSync(absPath + '/index.js')) {
-          return originalRequire.call(this, absPath + '/index.js');
+          return nativeRequire.call(this, absPath + '/index.js');
         } else if (fs.existsSync(absPath + '.json')) {
-          return originalRequire.call(this, absPath + '.json');
+          return nativeRequire.call(this, absPath + '.json');
         }
       }
     }
   }
-  return originalRequire.call(this, request);
+  return nativeRequire.call(this, request);
 };
 
 const standaloneServer = path.join(standaloneDir, 'server.js');
 
 if (fs.existsSync(standaloneServer)) {
   process.chdir(standaloneDir);
-
-  const rawPort = process.env.PORT;
-  if (rawPort && isNaN(Number(rawPort))) {
-    const origParseInt = global.parseInt;
-    global.parseInt = function(val, radix) {
-      if (val === rawPort) return rawPort;
-      return origParseInt(val, radix);
-    };
-    require(standaloneServer);
-    global.parseInt = origParseInt;
-  } else {
-    require(standaloneServer);
-  }
+  require(standaloneServer);
 } else {
   const { createServer } = require('http');
   const next = require('next');
